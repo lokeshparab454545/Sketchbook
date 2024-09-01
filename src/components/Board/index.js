c34da659840edaf7ad244ebada4b1c0cbd8d0972
Lokesh Parab <lokeshp@byteridge.com>
2024-09-01 11:39:15 +0530

    added draw line feature

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,9 +1,11 @@
 import { useRef } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useLayoutEffect } from "react";
 
 const Board = () => {
   const canvasRef = useRef(null);
+  const shouldDraw = useRef(false);
   const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
   const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
 
@@ -12,11 +14,47 @@ const Board = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
 
+    const changeConfig = () => {
+      context.strokeStyle = color;
+      context.lineWidth = size;
+    };
+
+    changeConfig();
+  }, [color, size]);
+
+  useLayoutEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    const handleMouseDown = (e) => {
+      shouldDraw.current = true;
+      context.beginPath();
+      context.moveTo(e.clientX, e.clientY);
+    };
+    const handleMouseMove = (e) => {
+      if (!shouldDraw.current) return;
+      context.lineTo(e.clientX, e.clientY);
+      context.stroke();
+    };
+    const handleMouseUp = (e) => {
+      shouldDraw.current = false;
+    };
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+    };
   }, []);
 
-  console.log(color, size);
   return <canvas ref={canvasRef}></canvas>;
 };
 export default Board;
